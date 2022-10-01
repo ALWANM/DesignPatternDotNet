f559481415dd36fd202df625e2197be6aaa41718
Marwan Alwan <marwanalwan94@gmail.com>
2022-10-01 20:23:17 +0200

    Intitialisation the project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection.Metadata;
+using System;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Hello World!"); 
+        } 
+    } 
+}
